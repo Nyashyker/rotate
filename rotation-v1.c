d322bc8d81f2beaa6c1ebf9624964f6d3b21eff1
Nyashyker <nyashyker@gmail.com>
2024-12-30 00:15:49 +0100

    fiexed

--- rotation-v1.c
+++ rotation-v1.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <limits.h>
+#include <math.h>
 
-#define BOX_SIZE 12
-
+#define null (void *)0
 
 typedef enum {
-    UP,//    = -1,
-    RIGHT,// = +1,
-    LEFT,//  = -1,
-    DOWN,//  = +1,
+    UP,
+    RIGHT,
+    LEFT,
+    DOWN,
 } Vector;
 
 typedef struct {
@@ -20,93 +21,98 @@ typedef struct {
 
 
 
+char* move_state_to_str(const Vector move_state);
 void show_matrix(const Cords *const buffer, const unsigned int max_box_size);
 Cords* clockwise_expanding_spiral_cords(Cords *const buffer, const unsigned int max_box_size);
 
 
 int main(int argc, char **argv)
 {
-    Cords* rez_buf = calloc(BOX_SIZE * BOX_SIZE, sizeof(Cords));
-    printf("Буфер виділився:\n");
-    show_matrix(rez_buf, BOX_SIZE);
+    unsigned int box_size;
+    if (argc == 2) {
+        const unsigned long tmp = strtoul(argv[1], null, 10);
+        if (tmp > UINT_MAX) { goto standard_size; }
+        box_size = atoi(argv[1]);
+    } else if (argc == 1) {
+        printf("Уведіть розмір сітки: ");
+        if (scanf("%iu", &box_size) != 1) { goto standard_size; }
+    } else {
+standard_size:
+        box_size = 13;
+    }
 
-    clockwise_expanding_spiral_cords(rez_buf, BOX_SIZE);
-    printf("Функція спрацювала...\n");
+    Cords* rez_buf = calloc(box_size * box_size, sizeof(Cords));
 
-    for (int i = 0; i < BOX_SIZE * 8 - 5; i++) { printf("-"); } printf("\n");
-    show_matrix(rez_buf, BOX_SIZE);
-    for (int i = 0; i < BOX_SIZE * 8 - 5; i++) { printf("-"); } printf("\n");
-    printf("Матриця успішно вивелася!\n");
-    printf("Програму завершено)\n");
+    clockwise_expanding_spiral_cords(rez_buf, box_size);
+    show_matrix(rez_buf, box_size);
 
+    free(rez_buf);
     return 0;
+}
+
+char* move_state_to_str(const Vector move_state)
+{
+    switch (move_state)
+    {
+        case UP: return "UP   ";
+        case RIGHT: return "RIGHT";
+        case LEFT: return "LEFT ";
+        case DOWN: return "DOWN ";
+    }
 }
 
 
 Cords* clockwise_expanding_spiral_cords(Cords *const buffer, const unsigned int max_box_size)
 {
-    const unsigned int center = max_box_size / 2;
+    const long center = max_box_size / 2 - !(max_box_size % 2);
 
     Cords position = {center, center, true};
+    Vector move_state = UP;
+    unsigned int imagine_box_size = 0;
 
-    // Стан руху по уявному квадрату із центру
-    Vector state = DOWN;
-    // розмір сторони квадрату (менша половина)
-    unsigned int imagine_matrix_size = 1;
-
-    // Уведення початку
-    buffer[0] = position;
-    position.x++;
-
-    for (size_t i = 1; i < max_box_size * max_box_size; i++)
+    for (size_t i = 0; i < max_box_size * max_box_size; i++)
     {
-        // Не даємо вийти за межі діапазону
-        if (imagine_matrix_size >= center) {
-            break;
-        }
-
         buffer[i] = position;
 
-        switch (state)
+        switch (move_state)
         {
-        case DOWN: // наразі рух: УНИЗ
-            if (position.y < center + imagine_matrix_size) {
-                position.y++;
-            } else { // повертаємо: НАЛІВО
-                state = LEFT;
-                position.x--;
-            }
-        break;
+            case DOWN: // наразі рух: УНИЗ
+                if (position.y < center + imagine_box_size) {
+                    position.y++;
+                } else { // повертаємо: НАЛІВО
+                    move_state = LEFT;
+                    position.x--;
+                }
+            break;
 
-        case LEFT: // наразі рух: НАЛІВО
-            if (position.x > center - imagine_matrix_size) {
-                position.x--;
-            } else { // повертаємо: УГОРУ
-                state = UP;
-                position.y--;
-            }
-        break;
+            case LEFT: // наразі рух: НАЛІВО
+                if (position.x > center - imagine_box_size) {
+                    position.x--;
+                } else { // повертаємо: УГОРУ
+                    move_state = UP;
+                    position.y--;
+                }
+            break;
 
-        case UP: // наразі рух: УГОРУ
-            if (position.y > center - imagine_matrix_size) {
-                position.y--;
-            } else { // повертаємо: НАПРАВО
-                state = RIGHT;
-                position.x++;
-            }
-        break;
+            case UP: // наразі рух: УГОРУ
+                if (position.y > center - imagine_box_size) {
+                    position.y--;
+                } else { // повертаємо: НАПРАВО
+                    move_state = RIGHT;
+                    position.x++;
+                }
+            break;
 
-        case RIGHT: // наразі рух: НАПРАВО
-            if (position.x <= center + imagine_matrix_size) {
-                position.x++;
-            } else { // повертаємо: УНИЗ
-                imagine_matrix_size++;
-                state = DOWN;
-                position.y++;
-            }
-        break;
+            case RIGHT: // наразі рух: НАПРАВО
+                if (position.x <= center + imagine_box_size) {
+                    position.x++;
+                } else { // повертаємо: УНИЗ
+                    imagine_box_size++;
+                    move_state = DOWN;
+                    position.y++;
+                }
+            break;
         }
-
     }
 
     return buffer;
@@ -123,10 +129,39 @@ void show_matrix(const Cords *const buffer, const unsigned int max_box_size)
         matrix[cords.y * max_box_size + cords.x] = i + 1;
     }
 
-    for (unsigned int y = 0; y < max_box_size; y++) {
-        for (unsigned int x = 0; x < max_box_size; x++) {
-            printf("%03d\t", matrix[y * max_box_size + x]);
+
+    const unsigned short num_len = log10(max_box_size * max_box_size) + 1;
+    const unsigned long border_size = max_box_size * num_len + (max_box_size - 1) * 2 + 2 * 2;
+
+
+    // Border
+    printf("+");
+    for (unsigned long i = 0; i < border_size; i++) {
+        if (i % (num_len + 2) > 1) {
+            printf("-");
+        } else {
+            printf(" ");
         }
-        printf("\n");
     }
+    printf("+\n\n");
+    // Matrix itself
+    for (unsigned int y = 0; y < max_box_size; y++) {
+        printf("|  ");
+        for (unsigned int x = 0; x < max_box_size; x++) {
+            printf("%0*d  ", num_len, matrix[y * max_box_size + x]);
+        }
+        printf("|\n\n");
+    }
+    // Border
+    printf("+");
+    for (unsigned long i = 0; i < border_size; i++) {
+        if (i % (num_len + 2) > 1) {
+            printf("-");
+        } else {
+            printf(" ");
+        }
+    }
+    printf("+\n");
+
+    free(matrix);
 }
